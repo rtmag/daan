dfc8e28b7c4a3d96017152e9fad71295ae1a717b
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-12 23:49:06 -0600

    Update 2_bowtie.bash

--- 2_bowtie.bash
+++ 2_bowtie.bash
@@ -29,3 +29,17 @@ bowtie /root/resources/hg38_bowtie/hg38 -p 30 -t -m 1 -S --chunkmbs 4000 \
 <( zcat /root/daan/fastq_trim/Ageing.YB.RNA.Rep2.20160606.R1.OK_trimmed.fq.gz ) \
 /root/daan/bowtie/YB2_uniq.sam
 ################################################################################################
+
+
+samtools view -bS HCT_siControl_HWN2YCCXX_L5_uniq.sam | samtools sort - HCT_siControl_HWN2YCCXX_L5_uniq
+samtools index HCT_siControl_HWN2YCCXX_L5_uniq.bam
+
+
+python /home/roberto/myPrograms/RepEnrich/RepEnrich.py \
+/home/roberto/references/hg19_repeatmasker_clean.txt \
+/home/roberto/deepa/novogene/repenrich HCT116_siC_DMSO \
+/home/roberto/references/RepEnrich_hg19/ \
+/home/roberto/deepa/novogene/repenrich_bowtie/HCT116_siC_DMSO_multimap_1.fastq \
+--fastqfile2 /home/roberto/deepa/novogene/repenrich_bowtie/HCT116_siC_DMSO_multimap_2.fastq \
+/home/roberto/deepa/novogene/repenrich_bowtie/HCT116_siC_DMSO_uniq.bam \
+--cpus 30 --pairedend TRUE
