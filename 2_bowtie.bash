d6ae92a9b14683f293cb3382dd87f1637d204443
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-15 00:50:38 -0600

    Update 2_bowtie.bash

--- 2_bowtie.bash
+++ 2_bowtie.bash
@@ -1,30 +1,32 @@
 ################################################################################################
-bowtie /root/resources/hg38_bowtie/hg38 -p 30 -t -m 1 -S --chunkmbs 4000 \
+bowtie-build /root/resources/tmp/mm10.fasta /root/resources/mm10_bowtie/mm10
+################################################################################################
+bowtie /root/resources/mm10_bowtie/mm10 -p 30 -t -m 1 -S --chunkmbs 4000 \
 --max /root/daan/bowtie/OB1_multimap.fastq \
 <( zcat /root/daan/fastq_trim/Ageing.OB.RNA.Rep1.20170522.R1.OK_trimmed.fq.gz ) \
 /root/daan/bowtie/OB1_uniq.sam
 
-bowtie /root/resources/hg38_bowtie/hg38 -p 30 -t -m 1 -S --chunkmbs 4000 \
+bowtie /root/resources/mm10_bowtie/mm10 -p 30 -t -m 1 -S --chunkmbs 4000 \
 --max /root/daan/bowtie/OB2_multimap.fastq \
 <( zcat /root/daan/fastq_trim/Ageing.OB.RNA.Rep2.20170522.R1.OK_trimmed.fq.gz ) \
 /root/daan/bowtie/OB2_uniq.sam
 
-bowtie /root/resources/hg38_bowtie/hg38 -p 30 -t -m 1 -S --chunkmbs 4000 \
+bowtie /root/resources/mm10_bowtie/mm10 -p 30 -t -m 1 -S --chunkmbs 4000 \
 --max /root/daan/bowtie/WE1_multimap.fastq \
 <( zcat /root/daan/fastq_trim/Ageing.WEHI.RNA.Rep1.20160606.R1.OK_trimmed.fq.gz ) \
 /root/daan/bowtie/WE1_uniq.sam
 
-bowtie /root/resources/hg38_bowtie/hg38 -p 30 -t -m 1 -S --chunkmbs 4000 \
+bowtie /root/resources/mm10_bowtie/mm10 -p 30 -t -m 1 -S --chunkmbs 4000 \
 --max /root/daan/bowtie/WE2_multimap.fastq \
 <( zcat /root/daan/fastq_trim/Ageing.WEHI.RNA.Rep2.20160606.R1.OK_trimmed.fq.gz ) \
 /root/daan/bowtie/WE2_uniq.sam
 
-bowtie /root/resources/hg38_bowtie/hg38 -p 30 -t -m 1 -S --chunkmbs 4000 \
+bowtie /root/resources/mm10_bowtie/mm10 -p 30 -t -m 1 -S --chunkmbs 4000 \
 --max /root/daan/bowtie/YB1_multimap.fastq \
 <( zcat /root/daan/fastq_trim/Ageing.YB.RNA.Rep1.20160606.R1.OK_trimmed.fq.gz ) \
 /root/daan/bowtie/YB1_uniq.sam
 
-bowtie /root/resources/hg38_bowtie/hg38 -p 30 -t -m 1 -S --chunkmbs 4000 \
+bowtie /root/resources/mm10_bowtie/mm10 -p 30 -t -m 1 -S --chunkmbs 4000 \
 --max /root/daan/bowtie/YB2_multimap.fastq \
 <( zcat /root/daan/fastq_trim/Ageing.YB.RNA.Rep2.20160606.R1.OK_trimmed.fq.gz ) \
 /root/daan/bowtie/YB2_uniq.sam
