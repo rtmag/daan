d9536abe5835d4ed499e78ea2ecf8b69cf9f10ec
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-18 09:54:34 -0600

    Update 2_bowtie.bash

--- 2_bowtie.bash
+++ 2_bowtie.bash
@@ -50,11 +50,3 @@ samtools view -bS YB2_uniq.sam | samtools sort - -o YB2_uniq.bam
 samtools index YB2_uniq.bam
 ################################################################################################
 
-python /home/roberto/myPrograms/RepEnrich/RepEnrich.py \
-/home/roberto/references/hg19_repeatmasker_clean.txt \
-/home/roberto/deepa/novogene/repenrich HCT116_siC_DMSO \
-/home/roberto/references/RepEnrich_hg19/ \
-/home/roberto/deepa/novogene/repenrich_bowtie/HCT116_siC_DMSO_multimap_1.fastq \
---fastqfile2 /home/roberto/deepa/novogene/repenrich_bowtie/HCT116_siC_DMSO_multimap_2.fastq \
-/home/roberto/deepa/novogene/repenrich_bowtie/HCT116_siC_DMSO_uniq.bam \
---cpus 30 --pairedend TRUE
