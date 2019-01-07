50b2067f163b8a4486fb8934a865768b15560d5d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-01-06 19:53:03 -0600

    Update 4_repenrich_edgeR.R

--- 4_repenrich_edgeR.R
+++ 4_repenrich_edgeR.R
@@ -170,6 +170,166 @@ boxplot(logFC ~ as.vector(repe), data=results, outline=FALSE, horizontal=TRUE,
 		  par(mar=c(6,10,4,1),cex.axis=1)
 	dev.off()
 }
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+
+library('DESeq2')
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+options(scipen=999)
+
+counts <- data.frame(
+  row.names = ob1[,1],  
+  ob1 = ob1[,4], we1 = we1[,4], 
+  yb1 = yb1[,4], ob2 = ob2[,4], 
+  we2 = we2[,4], yb2 = yb2[,4]
+)
+
+anno = ob1[,3][!ob1[,3] %in% c("srpRNA", "rRNA", "snRNA", "tRNA", "scRNA", "Satellite")]
+counts = counts[!ob1[,3] %in% c("srpRNA", "rRNA", "snRNA", "tRNA", "scRNA", "Satellite"), ]
+####################################################################################################################################
+# siC VS siK
+design<-data.frame(Treatment=c("OB","WE","YB","OB","WE","YB") )
+
+dds <- DESeqDataSetFromMatrix(countData = counts[,c(1,3,4,6)], colData = data.frame(Treatment=c("OB","YB","OB","YB")),
+			      design = ~ Treatment )
+dds <- DESeq(dds)
+dds_res = results(dds,contrast=c("Treatment","YB","OB"))
+table(dds_res$padj<0.05)
+table(is.na(dds_res$padj<0.05))
+####################################################################################################################################
+dLRT_vsd <- varianceStabilizingTransformation(dds)
+vsd = assay(dLRT_vsd)
+####################################################################################################################################
+# PCA
+pdf("deseq_pca.pdf")
+plotPCA(dLRT_vsd,ntop=50000,intgroup=c("Treatment"))
+dev.off()
+####################################################################################################################################
+# Volcano
+pdf("deseq_volcano_YOUNG_VS_OLD.pdf")
+plot(dds_res$log2FoldChange,-log10(dds_res$padj),xlab=expression('Log'[2]*' Fold Change ( Young OB / Old OB )'),
+              ylab=expression('-Log'[10]*' adjusted P-values'),col=alpha("grey",.5),pch=20)
+abline(v=-.5,lty = 2,col="grey")
+abline(v=.5,lty = 2,col="grey")
+abline(h=-log10(0.05),lty = 2,col="grey")
+points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>.5 & dds_res$padj<0.05],
+       -log10(dds_res$padj)[abs(dds_res$log2FoldChange)>.5 & dds_res$padj<0.05],
+      col="red",pch=20)
+  legend("topright", paste("Young OB",length(which(dds_res$log2FoldChange>.5 & dds_res$padj<0.05))), bty="n") 
+  legend("topleft", paste("Old OB",length(which(dds_res$log2FoldChange<(-.5) & dds_res$padj<0.05))), bty="n") 
+dev.off()
+####################################################################################################################################
+# tenames
+tenames=names(which(table(anno[which(dds_res$padj<0.05)])>5))
+
+track=as.character(anno[ which(dds_res$padj<0.05 & anno %in% tenames) ] )
+
+track[track=="ERVK"]=1
+track[track=="L1"]=2
+track[track=="ERV1"]=3
+track[track=="ERVL"]=4
+track[track=="ERVL-MaLR"]=5
+track[track=="Unknown"]=6
+track[track=="hAT-Charlie"]=7
+track[!track %in% c("1","2","3","4","5","6","7")]=8
+track=as.numeric(track)
+colores=c("#e6194b","#3cb44b","#ffe119","#0082c8","#f58231","#911eb4","grey","black")
+rlab = colores[track]
 
 
+sig_vsd = vsd[which( dds_res$padj<0.05 & anno %in% tenames),]
+colnames(sig_vsd) <- c("OB1","YB1","OB2","YB1")
+
+pdf("heatmaRE_YB_VS_OB.pdf")
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="Repetitive Elements",key.title="RE expression",cexCol=.8,RowSideColors=rlab)
+
+legend("topright",legend=c("ERVK","L1","ERV1","ERVL","ERVL-MaLR","Unknown","hAT-Charlie","other"),cex = 0.55,inset=c(-.01,-.01),
+       fill=c("#e6194b","#3cb44b","#ffe119","#0082c8","#f58231","#911eb4","grey","black"), border=T, bty="n" )
+dev.off()
+
+
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+
+library('DESeq2')
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+options(scipen=999)
+
+counts <- data.frame(
+  row.names = ob1[,1],  
+  ob1 = ob1[,4], we1 = we1[,4], 
+  yb1 = yb1[,4], ob2 = ob2[,4], 
+  we2 = we2[,4], yb2 = yb2[,4]
+)
+
+anno = ob1[,3][!ob1[,3] %in% c("srpRNA", "rRNA", "snRNA", "tRNA", "scRNA", "Satellite")]
+counts = counts[!ob1[,3] %in% c("srpRNA", "rRNA", "snRNA", "tRNA", "scRNA", "Satellite"), ]
+####################################################################################################################################
+# siC VS siK
+design<-data.frame(Treatment=c("OB","WE","YB","OB","WE","YB") )
+
+dds <- DESeqDataSetFromMatrix(countData = counts[,c(2,3,5,6)], colData = data.frame(Treatment=c("WE","YB","WE","YB")),
+			      design = ~ Treatment )
+dds <- DESeq(dds)
+dds_res = results(dds,contrast=c("Treatment","YB","WE"))
+table(dds_res$padj<0.05)
+table(is.na(dds_res$padj<0.05))
+####################################################################################################################################
+dLRT_vsd <- varianceStabilizingTransformation(dds)
+vsd = assay(dLRT_vsd)
+####################################################################################################################################
+####################################################################################################################################
+# Volcano
+pdf("deseq_volcano_YOUNG_VS_WEHI.pdf")
+plot(dds_res$log2FoldChange,-log10(dds_res$padj),xlab=expression('Log'[2]*' Fold Change ( Young OB / WEHI )'),
+              ylab=expression('-Log'[10]*' adjusted P-values'),col=alpha("grey",.5),pch=20)
+abline(v=-.5,lty = 2,col="grey")
+abline(v=.5,lty = 2,col="grey")
+abline(h=-log10(0.05),lty = 2,col="grey")
+points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>.5 & dds_res$padj<0.05],
+       -log10(dds_res$padj)[abs(dds_res$log2FoldChange)>.5 & dds_res$padj<0.05],
+      col="red",pch=20)
+  legend("topright", paste("Young OB",length(which(dds_res$log2FoldChange>.5 & dds_res$padj<0.05))), bty="n") 
+  legend("topleft", paste("Old OB",length(which(dds_res$log2FoldChange<(-.5) & dds_res$padj<0.05))), bty="n") 
+dev.off()
+####################################################################################################################################
+# tenames
+tenames=names(which(table(anno[which(dds_res$padj<0.05)])>5))
+
+track=as.character(anno[ which(dds_res$padj<0.05 & anno %in% tenames) ] )
+
+track[track=="ERVK"]=1
+track[track=="L1"]=2
+track[track=="ERV1"]=3
+track[track=="ERVL"]=4
+track[track=="ERVL-MaLR"]=5
+track[track=="Unknown"]=6
+track[track=="hAT-Charlie"]=7
+track[!track %in% c("1","2","3","4","5","6","7")]=8
+track=as.numeric(track)
+colores=c("#e6194b","#3cb44b","#ffe119","#0082c8","#f58231","#911eb4","grey","black")
+rlab = colores[track]
+
+
+sig_vsd = vsd[which( dds_res$padj<0.05 & anno %in% tenames),]
+colnames(sig_vsd) <- c("WE1","YB1","WE2","YB1")
+
+pdf("heatmaRE_YB_VS_WEHI.pdf")
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="Repetitive Elements",key.title="RE expression",cexCol=.8,RowSideColors=rlab)
+
+legend("topright",legend=c("ERVK","L1","ERV1","ERVL","ERVL-MaLR","Unknown","hAT-Charlie","other"),cex = 0.55,inset=c(-.01,-.01),
+       fill=c("#e6194b","#3cb44b","#ffe119","#0082c8","#f58231","#911eb4","grey","black"), border=T, bty="n" )
+dev.off()
 
