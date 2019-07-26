b98af345b3b4928d883b7bd9fa61411380ed8135
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-07-27 03:48:47 +0800

    Update 1_trimgalore.bash

--- chip_seq/1_trimgalore.bash
+++ chip_seq/1_trimgalore.bash
@@ -2,11 +2,23 @@ trim_galore --illumina -q 20 --fastqc -o /root/daan/fastq_trim/ \
 Ageing.OB.RNA.Rep1.20170522.R1.OK.fastq.gz &
 
 ##################
-#ALL Variant NO NORMAL
+#TRIM
 for bamfile in /root/daan/chip-seq/fastq/*fastq.gz ;
 do echo $bamfile; 
 name=${bamfile//\/root\/daan\/chip-seq\/fastq\/Ageing\.} ;
 sample=$(echo $name|cut -f1,2,3 -d'.') ;
 trim_galore --illumina -q 20 --fastqc -o /root/daan/chip-seq/fastq_trim/ $bamfile ;
+done
+##################
+##################
+#Bowtie & SAMTOOLS
+for fastq in /root/daan/chip-seq/fastq_trim/*_trimmed.fq.gz ;
+do echo $fastq; 
+name=${fastq//\/root\/daan\/chip-seq\/fastq_trim\/Ageing\.} ;
+name=${name//\.R1\_trimmed\.fq\.gz} ;
+bowtie /root/resources/mm10_bowtie/mm10 -p 30 -t -m 1 -S --chunkmbs 4000 \
+--max /root/daan/chip-seq/bowtie/$name_multimap.fastq \
+<( zcat $fastq ) \
+/root/daan/chip-seq/bowtie/$name_uniq.sam &> /root/daan/chip-seq/bowtie/$name.bowtie.log ;
 done
 ##################
